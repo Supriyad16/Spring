d48e3285f1edf4f2fd9b356eccc1872a3c8145df
Supriya D <dinakaranctsupriya@gmail.com>
2025-09-17 12:22:09 +0530

    Hospital Website

---
+++ Hospital/src/main/java/com/xworkz/hospital/entity/SlotEntity.java
@@ -0,0 +1,33 @@
+package com.xworkz.hospital.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Entity
+@Table(name = "slot_data")
+
+@NamedQuery(name = "getAllSlots", query = "SELECT s FROM SlotEntity s")
+public class SlotEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String fromTime;
+
+    private String toTime;
+
+    @Transient
+    public String getSlotDisplay() {
+        return fromTime + " - " + toTime;
+    }
+
+}
