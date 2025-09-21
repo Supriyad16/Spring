aedfd61221cdabab98aa9c71d07ab63be244ab5f
Supriya D <dinakaranctsupriya@gmail.com>
2025-09-21 23:23:57 +0530

    Hospital Website

---
+++ Hospital/src/main/java/com/xworkz/hospital/dto/SpecialsationDTO.java
@@ -0,0 +1,15 @@
+package com.xworkz.hospital.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class SpecialsationDTO {
+
+    private String Specialisation;
+
+}
