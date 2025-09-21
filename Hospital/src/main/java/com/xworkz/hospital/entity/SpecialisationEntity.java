23815015a5612e66df4590b17665485cf012503b
Supriya D <dinakaranctsupriya@gmail.com>
2025-09-21 23:23:44 +0530

    Hospital Website

---
+++ Hospital/src/main/java/com/xworkz/hospital/entity/SpecialisationEntity.java
@@ -0,0 +1,28 @@
+package com.xworkz.hospital.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+
+@Entity
+@Table(name="specialsation_data")
+
+
+public class SpecialisationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "specialisation")
+    private String specialisation;
+
+
+}
