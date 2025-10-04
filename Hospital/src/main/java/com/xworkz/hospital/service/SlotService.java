9fc646652995dc36913887d585c4a64fc00f805f
Supriya D <dinakaranctsupriya@gmail.com>
2025-10-05 01:59:59 +0530

    Hospital Website

--- Hospital/src/main/java/com/xworkz/hospital/service/SlotService.java
+++ Hospital/src/main/java/com/xworkz/hospital/service/SlotService.java
@@ -1,4 +1,15 @@
 package com.xworkz.hospital.service;
 
-public class SlotService {
+import com.xworkz.hospital.dto.SlotDTO;
+import com.xworkz.hospital.entity.SpecialisationEntity;
+
+import java.util.List;
+
+public interface SlotService {
+
+    List<SpecialisationEntity> getAllSpecialisation();
+
+    SpecialisationEntity getById(int id);
+
+    boolean saveSlot(SlotDTO slotDTO);
 }
--- Hospital/src/main/java/com/xworkz/hospital/service/SlotServiceImp.java
+++ Hospital/src/main/java/com/xworkz/hospital/service/SlotServiceImp.java
@@ -1,4 +1,38 @@
 package com.xworkz.hospital.service;
 
-public class SlotServiceImp {
+import com.xworkz.hospital.dto.SlotDTO;
+import com.xworkz.hospital.entity.SlotEntity;
+import com.xworkz.hospital.entity.SpecialisationEntity;
+import com.xworkz.hospital.repository.SlotRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SlotServiceImp implements SlotService {
+
+    @Autowired
+    SlotRepository slotRepository;
+
+
+    @Override
+    public List<SpecialisationEntity> getAllSpecialisation() {
+        System.out.println("Specialisations fetched from DB: " + slotRepository.getAllSpecialisations());
+        return slotRepository.getAllSpecialisations();
+    }
+
+    @Override
+    public SpecialisationEntity getById(int id) {
+        return slotRepository.getById(id);
+    }
+
+    @Override
+    public boolean saveSlot(SlotDTO slotDTO) {
+        SlotEntity slotEntity = new SlotEntity();
+        BeanUtils.copyProperties(slotDTO, slotEntity);
+        return slotRepository.saveSlot(slotEntity);
+    }
+
 }
