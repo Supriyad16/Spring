8d34e0ecb1e623d79bb6e1840c47d8f91b8801d3
Supriya D <dinakaranctsupriya@gmail.com>
2025-10-04 23:54:25 +0530

    Hospital Website

--- Hospital/src/main/java/com/xworkz/hospital/service/SpecialisationService.java
+++ Hospital/src/main/java/com/xworkz/hospital/service/SpecialisationService.java
@@ -1,4 +1,8 @@
 package com.xworkz.hospital.service;
 
-public class SpecialisationService {
+import com.xworkz.hospital.dto.SpecialsationDTO;
+
+public interface SpecialisationService {
+
+    boolean specialisationSave(SpecialsationDTO specialsationDTO);
 }
--- Hospital/src/main/java/com/xworkz/hospital/service/SpecialisationServiceImp.java
+++ Hospital/src/main/java/com/xworkz/hospital/service/SpecialisationServiceImp.java
@@ -1,4 +1,27 @@
 package com.xworkz.hospital.service;
 
-public class SpecialisationServiceImp {
+import com.xworkz.hospital.dto.SpecialsationDTO;
+import com.xworkz.hospital.entity.SpecialisationEntity;
+import com.xworkz.hospital.repository.SpecialisationRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SpecialisationServiceImp implements SpecialisationService {
+
+    @Autowired
+    private SpecialisationRepository specialisationRepository;
+
+    @Override
+    public boolean specialisationSave(SpecialsationDTO specialsationDTO) {
+
+        SpecialisationEntity specialisationEntity = new SpecialisationEntity();
+        BeanUtils.copyProperties(specialsationDTO, specialisationEntity);
+        return  specialisationRepository.specialisationSave(specialisationEntity);
+
+    }
+
+
 }
