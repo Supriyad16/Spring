630cc11f9ac58b130cbfeb52eb7c12fe43a081c1
Supriya D <dinakaranctsupriya@gmail.com>
2025-10-14 22:37:41 +0530

    Hospital Website

--- Hospital/src/main/java/com/xworkz/hospital/configuration/HospitalInitializer.java
+++ Hospital/src/main/java/com/xworkz/hospital/configuration/HospitalInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class HospitalInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
