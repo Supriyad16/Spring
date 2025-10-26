e6d93390e9184dc64e6c30dd2e2deaa3f7c1f271
Supriya D <dinakaranctsupriya@gmail.com>
2025-10-26 23:31:52 +0530

    Hospital Website

--- Hospital/src/main/java/com/xworkz/hospital/dto/PatientDTO.java
+++ Hospital/src/main/java/com/xworkz/hospital/dto/PatientDTO.java
@@ -2,6 +2,8 @@ package com.xworkz.hospital.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
@@ -15,14 +17,24 @@ public class PatientDTO {
 
     private String registrationId;
 
+    @NotNull
+    @Size(min = 3, max = 20, message = "Name Should Be Between 3 and 20")
     private String patientName;
 
+    @NotNull(message = "gender should not be empty")
     private String gender;
 
+    @NotNull
+    @Min(12)
+    @Max(60)
     private int age;
 
+    @NotNull(message = "Phone number is required")
+    @Min(value = 1000000000L, message = "Phone number must be 10 digits")
+    @Max(value = 9999999999L, message = "Phone number must be 10 digits")
     private long phoneNumber;
 
+    @Pattern(regexp = "^[A-Za-z0-9]+@gmail\\.com$", message = "Invalid format")
     private String email;
 
     private String address;
